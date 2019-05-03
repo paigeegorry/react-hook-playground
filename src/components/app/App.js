6048b8f4eb00252b11c54fccb7f86506b35142e3
Paige Gorry <paigeegorry@gmail.com>
2019-05-03 11:37:33 -0700

    basic form updating with hooks!

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Form from '../form/Form';
 
 export default function App() {
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>Hello</h1>
+      <Form />
+    </>
   );
 }
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function Form() {
+export default function Button() {
+  const [title, setTitle] = useState('');
+  const [color, setColor] = useState('black');
+
   return (
-    <input type="text" />
+    <div>
+      <h2 style={{ color }}>{title}</h2>
+      <input type="text" name="title" value={title} onChange={({ target }) => setTitle(target.value)}/>
+      <input type="color" name="color" value={color} onChange={({ target }) => setColor(target.value)} />
+    </div>
   );
 }
